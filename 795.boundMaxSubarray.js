8b0a8b6c77f076c1629407b535fce0f20e2db7c5
mofas223 <mofas223@gmail.com>
2018-05-21 13:24:35 +0800

    finish my solution

--- 795.boundMaxSubarray.js
+++ 795.boundMaxSubarray.js
@@ -42,9 +42,10 @@ const startFrom = (A, L, R) => {
 // console.log(startFrom([4, 1], 3, 4)); // [4] [4, 1]
 // console.log(startFrom([4, 1, 1], 3, 4)); // [4] [4, 1] [4, 1, 1]
 // console.log(startFrom([4, 1, 3], 3, 4)); // [4] [4, 1] [4, 1, 3]
-// console.log(startFrom([1, 1, 4], 3, 4)); // [1, 3] [1, 3, 4]
+// console.log(startFrom([1, 1, 4], 3, 4)); // [1, 1, 4]
 // console.log(startFrom([3, 4, 1], 3, 4)); // [3] [3, 4] [3, 4, 1]
 // console.log(startFrom([3, 1, 1, 4], 3, 4)); // [ [ 3 ], [ 3, 1 ], [ 3, 1, 1 ], [ 3, 1, 1, 4 ] ]
+// console.log(startFrom([1, 1, 4, 2, 3], 3, 4)); // [1, 1, 4] [1, 1, 4, 2] [1, 1, 4, 2, 3]
 
 // the ele in A will be smaller than R
 const stepGen = (A, L, R) => {
@@ -56,7 +57,7 @@ const stepGen = (A, L, R) => {
   return ret;
 };
 
-var numSubarrayBoundedMax = function(A, L, R) {
+var numSubarrayBoundedMaxResult = function(A, L, R) {
   const len = A.length;
 
   if (len === 0) return [];
@@ -76,7 +77,61 @@ var numSubarrayBoundedMax = function(A, L, R) {
   return ret;
 };
 
-// console.log(numSubarrayBoundedMax([1, 2, 3, 4], 1, 4)); // 10
+// console.log(numSubarrayBoundedMaxResult([1, 2, 3, 4], 1, 4)); // 10
+// // // [1] [1, 2] [1, 2, 3] [1, 2, 3, 4] [2] [2, 3] [2, 3, 4] [3] [3, 4] [4]
+
+// console.log(numSubarrayBoundedMaxResult([3, 1, 1, 3], 2, 3)); // 7
+// // // [3], [3, 1], [3, 1, 1], [3, 1, 1, 3], [1, 1, 3], [1, 3], [3]
+
+// console.log(numSubarrayBoundedMaxResult([3, 1, 1, 9], 2, 3)); // 3
+// // // [3], [3, 1], [3, 1, 1]
+
+// console.log(numSubarrayBoundedMaxResult([2, 1, 4, 3], 2, 3)); // 3
+// // // // [2], [2, 1], [3]
+
+// console.log(numSubarrayBoundedMaxResult([1, 5, 9, 4, 3], 4, 6)); // 4
+// // // // // [1, 5], [5], [4], [4, 3]
+
+// console.log(numSubarrayBoundedMaxResult([1, 9, 4, 3], 4, 6)); // 2
+// // [4], [4, 3]
+
+// console.log(numSubarrayBoundedMaxResult([2, 9, 2, 5, 6], 2, 8)); // 7
+// // // [2] [2] [2, 5] [2, 5, 6] [5] [5, 6] [6]
+
+const stepCount = (A, L) => {
+  const len = A.length;
+  let ret = 0;
+  for (let i = 0; i < len; i++) {
+    for (let j = i; j < len; j++) {
+      if (A[j] >= L) {
+        ret += len - j;
+        break;
+      }
+    }
+  }
+  return ret;
+};
+
+var numSubarrayBoundedMax = function(A, L, R) {
+  const len = A.length;
+
+  if (len === 0) return 0;
+
+  let ret = 0;
+  let last = 0;
+  for (let i = 0; i < len; i++) {
+    if (A[i] > R) {
+      const left = A.slice(last, i);
+      ret += stepCount(left, L);
+      last = i + 1;
+    }
+  }
+  const right = A.slice(last, len);
+  ret += stepCount(right, L);
+  return ret;
+};
+
+console.log(numSubarrayBoundedMax([1, 2, 3, 4], 1, 4)); // 10
 // // [1] [1, 2] [1, 2, 3] [1, 2, 3, 4] [2] [2, 3] [2, 3, 4] [3] [3, 4] [4]
 
 console.log(numSubarrayBoundedMax([3, 1, 1, 3], 2, 3)); // 7
