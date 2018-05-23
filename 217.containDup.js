b0b9976850fc8fc9e9783a3237f5914e6bdc6738
mofas223 <mofas223@gmail.com>
2018-05-23 22:38:40 +0800

    add 217

---
+++ 217.containDup.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  const n = nums.length;
+  const hash = {};
+
+  for (let i = 0; i < n; i++) {
+    const d = nums[i];
+    if (hash[d]) {
+      return true;
+    }
+    hash[d] = true;
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1])); //true
+console.log(containsDuplicate([1, 2, 3, 4])); //false
