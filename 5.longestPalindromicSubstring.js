f221fe679553f35acd1c6653298e0141691ce8fc
mofas223 <mofas223@gmail.com>
2018-06-04 20:38:46 +0800

    heavy optimized 5

--- 5.longestPalindromicSubstring.js
+++ 5.longestPalindromicSubstring.js
@@ -2,24 +2,34 @@
  * @param {string} s
  * @return {string}
  */
+
+// heavy optimized
 var longestPalindrome = function(s) {
   let from = 0;
   let to = 0;
 
-  for (let i = 0; i < s.length; i++) {
-    expand(i, i);
-    expand(i, i + 1);
-  }
+  const n = s.length;
 
-  function expand(i, j) {
-    let count = 0;
-    while (i >= 0 && j < s.length && s[i] === s[j]) {
-      i--;
-      j++;
+  if (n === 0) return '';
+  if (n == 1) return s;
+
+  for (let i = 0; i < n; i++) {
+    if (n - i <= (to - from) / 2) break;
+
+    let j = i;
+    let k = i;
+
+    // skip duplicate
+    while (k < n - 1 && s[k + 1] === s[k]) k++;
+    i = k;
+
+    while (j >= 0 && k < s.length && s[j] === s[k]) {
+      j--;
+      k++;
     }
-    if (j - i > to - from) {
-      from = i;
-      to = j;
+    if (k - j > to - from) {
+      from = j;
+      to = k;
     }
   }
 
@@ -43,7 +53,6 @@ var longestPalindrome = function(s) {
     }
     end = i;
     len = end - start + 1;
-    // console.log(len);
     var offstart = start;
     var offend = end;
     for (
@@ -67,6 +76,23 @@ var longestPalindrome = function(s) {
 
   return s.substring(first, first + Maxlen);
 };
+
+string longestPalindrome(string s) {
+    if (s.empty()) return "";
+    if (s.size() == 1) return s;
+    int min_start = 0, max_len = 1;
+    for (int i = 0; i < s.size();) {
+      if (s.size() - i <= max_len / 2) break;
+      int j = i, k = i;
+      while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
+      i = k+1;
+      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+      int new_len = k - j + 1;
+      if (new_len > max_len) { min_start = j; max_len = new_len; }
+    }
+    return s.substr(min_start, max_len);
+}
+
  */
 
 console.log(longestPalindrome('babad'));
