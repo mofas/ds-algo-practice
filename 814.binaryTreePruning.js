1d6730faa3eb8de353b019813686e0ec263c1ab2
mofas223 <mofas223@gmail.com>
2018-07-01 19:30:38 -0400

    add 814

--- 814.binaryTreePruning.js
+++ 814.binaryTreePruning.js
@@ -6,9 +6,23 @@ const { TreeNode, buildTree, printTree } = treeUtil;
  * @return {TreeNode}
  */
 var pruneTree = function(root) {
-  //
+  if (root) {
+    let lb = pruneTree(root.left);
+    let rb = pruneTree(root.right);
+    if (lb || rb || root.val !== 0) {
+      root.left = lb;
+      root.right = rb;
+      return root;
+    }
+  }
+  return null;
 };
 
-// console.log(printTree(pruneTree(buildTree([1, null, 0, 0, 1]))));
+console.log(printTree(pruneTree(buildTree([1, null, 0, 0, 1]))));
+// [1,null,0,null,1]
 
-console.log(buildTree([1, null, 0, 0, 1]));
+console.log(printTree(pruneTree(buildTree([1, 0, 1, 0, 0, 0, 1]))));
+// [1,null,1,null,1]
+
+console.log(printTree(pruneTree(buildTree([1, 1, 0, 1, 1, 0, 1, 0]))));
+// [1, 1, 0, 1, 1, null, 1];
