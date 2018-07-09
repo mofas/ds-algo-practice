d632ea5e0fbd9a42cc14f0255d8a23d9093952d5
mofas223 <mofas223@gmail.com>
2018-07-09 09:48:33 -0400

    add 771

---
+++ 771.jewelsStones.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+  const hash = new Set();
+  for (let i = 0; i < J.length; i++) {
+    hash.add(J.substring(i, i + 1));
+  }
+  let ret = 0;
+  for (let i = 0; i < S.length; i++) {
+    if (hash.has(S.substring(i, i + 1))) ret++;
+  }
+  return ret;
+};
+
+// best sol from web
+// var numJewelsInStones = function(J, S) {
+//   const jewels = J.split('');
+//   return S.split('').reduce(function(a, b) {
+//     if (jewels.includes(b)) {
+//       a += 1;
+//     }
+//     return a;
+//   }, 0);
+// };
+
+console.log(numJewelsInStones('aA', 'aAAbbbb'));
+// 3
+
+console.log(numJewelsInStones('z', 'ZZ'));
+// 0
