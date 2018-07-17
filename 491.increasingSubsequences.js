0b774377d9f07f71c77c0452e602375178458f18
mofas223 <mofas223@gmail.com>
2018-07-17 15:22:45 -0400

    add 802

--- 491.increasingSubsequences.js
+++ 491.increasingSubsequences.js
@@ -6,63 +6,63 @@
 // using hashmap
 // it may be slow
 // 292ms
-var findSubsequences = function(nums) {
-  const n = nums.length;
-  let ret = [];
-  let temp = [];
-  const seen = new Set();
-  const combs = new Set();
-  for (let i = 0; i < n; i++) {
-    temp = [];
-    if (!seen.has(nums[i])) {
-      temp.push([nums[i]]);
-    }
-    for (let j = 0; j < ret.length; j++) {
-      const t = ret[j];
-      const lastEle = t[t.length - 1];
-      if (nums[i] >= lastEle) {
-        const newComb = t.concat([nums[i]]);
-        if (!combs.has(newComb.join(','))) {
-          temp.push(newComb);
-          combs.add(newComb.join(','));
-        }
-      }
-    }
-    temp = ret.concat(temp);
-    ret = temp;
-    seen.add(nums[i]);
-  }
+// var findSubsequences = function(nums) {
+//   const n = nums.length;
+//   let ret = [];
+//   let temp = [];
+//   const seen = new Set();
+//   const combs = new Set();
+//   for (let i = 0; i < n; i++) {
+//     temp = [];
+//     if (!seen.has(nums[i])) {
+//       temp.push([nums[i]]);
+//     }
+//     for (let j = 0; j < ret.length; j++) {
+//       const t = ret[j];
+//       const lastEle = t[t.length - 1];
+//       if (nums[i] >= lastEle) {
+//         const newComb = t.concat([nums[i]]);
+//         if (!combs.has(newComb.join(','))) {
+//           temp.push(newComb);
+//           combs.add(newComb.join(','));
+//         }
+//       }
+//     }
+//     temp = ret.concat(temp);
+//     ret = temp;
+//     seen.add(nums[i]);
+//   }
 
-  return ret.filter(d => d.length > 1);
-};
+//   return ret.filter(d => d.length > 1);
+// };
 
 // the best sol from web
 // 224ms
 // it uses backtracking
-// const findSubsequences = nums => {
-//   const res = [];
-//   const seq = [];
-//   dfs(res, seq, nums, 0);
-//   return res;
-// };
+const findSubsequences = nums => {
+  const res = [];
+  const seq = [];
+  dfs(res, seq, nums, 0);
+  return res;
+};
 
-// const dfs = (res, seq, nums, pos) => {
-//   if (seq.length > 1) {
-//     res.push(seq.slice());
-//   }
-//   let set = new Set();
-//   for (let i = pos; i < nums.length; i += 1) {
-//     if (
-//       (seq.length === 0 || seq[seq.length - 1] <= nums[i]) &&
-//       !set.has(nums[i])
-//     ) {
-//       seq.push(nums[i]);
-//       dfs(res, seq, nums, i + 1);
-//       seq.pop();
-//       set.add(nums[i]);
-//     }
-//   }
-// };
+const dfs = (res, seq, nums, pos) => {
+  if (seq.length > 1) {
+    res.push(seq.slice());
+  }
+  let set = new Set();
+  for (let i = pos; i < nums.length; i += 1) {
+    if (
+      (seq.length === 0 || seq[seq.length - 1] <= nums[i]) &&
+      !set.has(nums[i])
+    ) {
+      seq.push(nums[i]);
+      dfs(res, seq, nums, i + 1);
+      seq.pop();
+      set.add(nums[i]);
+    }
+  }
+};
 
 console.log(findSubsequences([1, 2, 3, 4, 5]));
 
