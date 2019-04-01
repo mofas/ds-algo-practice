af6553232014ef0e8af7ebd6d2e05f421c9d70b0
mofas223 <mofas223@gmail.com>
2019-04-01 10:51:03 -0700

    add 412
    Signed-off-by: mofas223 <mofas223@gmail.com>

---
+++ js/412.fizzBuzz.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  let ret = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) ret.push('FizzBuzz');
+    else if (i % 3 === 0) ret.push('Fizz');
+    else if (i % 5 === 0) ret.push('Buzz');
+    else ret.push(i + '');
+  }
+  return ret;
+};
